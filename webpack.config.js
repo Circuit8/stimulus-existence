6ca5211e302bb40311f46c4ac41596784137646b
Joshua Harris <joshuaweharris@googlemail.com>
2020-06-23 16:50:59 +0100

    Fixed webpack bug

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,29 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'stimulus-toggle.js',
-    library: 'stimulusToggle',
-    libraryTarget: 'umd'
+    path: path.resolve(__dirname, "dist"),
+    filename: "stimulus-existence.js",
+    library: "stimulusExistence",
+    libraryTarget: "umd",
   },
   externals: {
     stimulus: {
-      commonjs: 'stimulus',
-      commonjs2: 'stimulus',
-      amd: 'stimulus',
-      root: 'stimulus'
-    }
+      commonjs: "stimulus",
+      commonjs2: "stimulus",
+      amd: "stimulus",
+      root: "stimulus",
+    },
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        include: path.resolve(__dirname, 'src'),
+        include: path.resolve(__dirname, "src"),
         exclude: [/node_modules/],
-        use: [
-          { loader: 'babel-loader' }
-        ]
-      }
-    ]
-  }
-}
+        use: [{ loader: "babel-loader" }],
+      },
+    ],
+  },
+};
